493e42b5269a71c96ccfd224dc7b6ea1b75386db
takaman <takaman@gmail.com>
2025-12-23 12:41:05 +0200

    Add contract artifacts to repo for Vercel build

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    fs: {
+      // Allow serving files from the contracts package
+      allow: ['..']
+    }
+  }
 })
